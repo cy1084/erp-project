9d2f2b145f84ab54fccb5772ab1e4bcab1c017f1
cy1084 <dyddyd96@skuniv.ac.kr>
2023-05-20 15:29:45 +0900

    test

--- src/main/java/com/erp/chae/service/CustomerInfoService.java
+++ src/main/java/com/erp/chae/service/CustomerInfoService.java
@@ -10,36 +10,30 @@ import com.erp.chae.vo.CustomerInfoVO;
 
 @Service
 public class CustomerInfoService {
-		@Autowired
-		private CustomerInfoMapper customerInfoMapper;
+	@Autowired
+	private CustomerInfoMapper customerInfoMapper;
 
-		public List<CustomerInfoVO> getCustomerInfos(CustomerInfoVO customerInfoVO){
-			return customerInfoMapper.getCustomerInfos(customerInfoVO);
-		}
-		
-		public CustomerInfoVO getCustomerInfo(CustomerInfoVO customerInfoVO){
-			return customerInfoMapper.getCustomerInfo(customerInfoVO);
-		}
-		
-		public boolean insertCustomerInfo(CustomerInfoVO customerInfoVO){
-			if(customerInfoMapper.getCustomerInfo(customerInfoVO)!=null) { 
-				return false; 
-			}
-			return customerInfoMapper.insertCustomerInfo(customerInfoVO) == 1;
-		}
-		
-		public boolean updateCustomerInfo(CustomerInfoVO customerInfoVO){
-			if(customerInfoMapper.getCustomerInfo(customerInfoVO)!=null) { 
-				return false; 
-			}
-			return customerInfoMapper.updateCustomerInfo(customerInfoVO) == 1;
-		}
-		
-		public boolean deleteCustomerInfo(CustomerInfoVO customerInfoVO){
-			return customerInfoMapper.deleteCustomerInfo(customerInfoVO) == 1;
-		}
-
+	public List<CustomerInfoVO> getCustomerInfos(CustomerInfoVO customerInfoVO) {
+		return customerInfoMapper.getCustomerInfos(customerInfoVO);
 	}
 
+	public CustomerInfoVO getCustomerInfo(CustomerInfoVO customerInfoVO) {
+		return customerInfoMapper.getCustomerInfo(customerInfoVO);
+	}
 
+	public boolean insertCustomerInfo(CustomerInfoVO customerInfoVO) {
+		if (customerInfoMapper.getCustomerInfo(customerInfoVO) != null) {
+			return false;
+		}
+		return customerInfoMapper.insertCustomerInfo(customerInfoVO) == 1;
+	}
 
+	public boolean updateCustomerInfo(CustomerInfoVO customerInfoVO) {
+		return customerInfoMapper.updateCustomerInfo(customerInfoVO) == 1;
+	}
+
+	public boolean deleteCustomerInfo(CustomerInfoVO customerInfoVO) {
+		return customerInfoMapper.deleteCustomerInfo(customerInfoVO) == 1;
+	}
+
+}
