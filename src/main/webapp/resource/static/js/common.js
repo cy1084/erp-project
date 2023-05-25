90621e52196daaed87a8c3812a345c70e2b5e2e5
Administrator <Administrator@DESKTOP-E9AN2K9>
2023-05-25 15:28:24 +0900

    pagenation 수정

--- src/main/webapp/resource/static/js/common.js
+++ src/main/webapp/resource/static/js/common.js
@@ -1,16 +1,16 @@
 /**
  * 
  */
-function goPage(pathname){
-	location.href=pathname;
+function goPage(pathName){
+	location.href=pathName;
 }
 //data-total="${page.total}" data-page-num="${page.pageNum}" data-block-size="10
 window.addEventListener('DOMContentLoaded', function(){
-	const {pathname} = window.location;
+	let pathName = window.location.href;
 	const menus = document.querySelectorAll('li.sidebar-item[data-menu]');
 	for(const menu of menus){
 		menu.classList.remove('active');
-		if(pathname === menu.getAttribute('data-menu')){
+		if(pathName === menu.getAttribute('data-menu')){
 			menu.classList.add('active');
 		}
 	}
@@ -20,32 +20,43 @@ window.addEventListener('DOMContentLoaded', function(){
 		if(pages==='0'){
 			return;
 		}
-		const pathname = window.location.href;
+		
+		if(!pathName.includes('?')){
+			pathName += '?';
+		}
 		const pagePrefix = paginationObj.getAttribute('data-page-prefix')?paginationObj.getAttribute('data-page-prefix'):'';
-		const pageStr = pagePrefix + 'page';
+		let pageStr = pagePrefix + 'page';
+		if(pathName.includes(pageStr)){
+			const regex = new RegExp(`&${pageStr}=+[0-9]*`, 'g');
+			pathName = pathName.replace(regex,'');
+		}
+		pathName += '&' + pageStr + '=';
 		const pageNum = paginationObj.getAttribute('data-page-num');
 		const startBlock = Math.floor((pageNum - 1)/10)*10+1;
 		const beforBlock = startBlock!=1?startBlock-1:1;
 		const endBlock = (startBlock + 9)> pages ? pages : startBlock + 9;
 		const afterBlock = endBlock!=pages?endBlock+1:endBlock;
 		let html = '							<nav aria-label="Page navigation example" id="pagination"  >'
-				+ '							      <ul class=\'pagination justify-content-center\'>\n'
-				+ '										<li class=\'page-item\'>\n'
-				+ '											<a class=\'page-link\' href=\'' +pathname +'&' + pageStr + '=' + (beforBlock) +'\' aria-label=\'Previous\'> \n'
+				+ '							      <ul class=\'pagination justify-content-center\'>\n';
+		if(pageNum!=1){
+			html +='										<li class=\'page-item\'>\n'
+				+ '											<a class=\'page-link\' href=\'' +pathName+ (beforBlock) +'\' aria-label=\'Previous\'> \n'
 				+ '												<span aria-hidden=\'true\'>&laquo;</span>\n'
 				+ '											</a>\n'
 				+ '										</li>\n';
-		for(let i=startBlock;i<=endBlock;i++){
-				const active = i==pageNum?'active':''
-				html += '										<li class=\'page-item ' + active + '\'><a class=\'page-link\' href=\'' +pathname +'&' + pageStr + '=' + i +'\'>' + i + '</a></li>\n'
 		}
-				
-		html += '										<li class=\'page-item\'>\n'
-				+ '											<a class=\'page-link\' href=\'' +pathname +'&' + pageStr + '=' + (afterBlock) +'\' aria-label=\'Next\'> \n'
+		for(let i=startBlock;i<=endBlock;i++){
+			const active = i==pageNum?'active':''
+			html += '										<li class=\'page-item ' + active + '\'><a class=\'page-link\' href=\'' +pathName+ i +'\'>' + i + '</a></li>\n'
+		}
+		if(pageNum!=endBlock){
+			html += '										<li class=\'page-item\'>\n'
+				+ '											<a class=\'page-link\' href=\'' +pathName+ (afterBlock) +'\' aria-label=\'Next\'> \n'
 				+ '												<span aria-hidden=\'true\'>&raquo;</span>\n'
 				+ '											</a>\n'
-				+ '										</li>\n'
-				+ '									</ul>'
+				+ '										</li>\n';
+		}
+		html += '									</ul>'
 				+ ' 							</nav>';
 		paginationObj.insertAdjacentHTML('afterend', html);		
 		
